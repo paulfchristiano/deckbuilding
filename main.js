d002683ef77548eb5bd82df135c0dfb9205b8ada
paulfchristiano <paulfchristiano@gmail.com>
2020-04-26 16:27:08 -0700

    turn off testing

--- main.js
+++ main.js
@@ -1412,7 +1412,6 @@ const twins = new Card('Twins', {
     }]
 })
 mixins.push(makeCard(twins, {time:0, coin:6}))
-testing.push(makeCard(twins, {time:0, coin:6}))
 
 const masterSmith = new Card('Master Smith', {
     fixedCost: time(2),
