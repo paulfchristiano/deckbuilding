d4d97be9bc1d489abd859c9f0b84ce58266eedf5
paulfchristiano <paulfchristiano@gmail.com>
2020-05-09 12:07:23 -0700

    database test

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const pool = new Pool({
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .set('view engine', 'ejs')
+    .get('/db', async (req, res) => {
+      try {
+          const client = await pool.connect();
+          const result = await client.query('SELECT * FROM scoreboard');
+          const results = { 'results': (result) ? result.rows : null };
+          res.render('pages/scoreboard', results);
+          client.release()
+      } catch(err) {
+          console.error(err);
+          res.send('Error ' + err);
+      }
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 //
 //  .set('views', path.join(__dirname, 'views'))
