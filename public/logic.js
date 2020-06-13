0a4df85d7299329a0b24b2057761eeaf3a9b9a3f
paulfchristiano <paulfchristiano@gmail.com>
2020-06-13 15:12:19 -0700

    bump version number

--- public/logic.js
+++ public/logic.js
@@ -85,7 +85,7 @@ var __read = (this && this.__read) || function (o, n) {
     }
     return ar;
 };
-export var VERSION = "0.6.3";
+export var VERSION = "0.7";
 // ----------------------------- Formatting
 export function renderCost(cost, full) {
     var e_1, _a;
